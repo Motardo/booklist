d638daf87d4e909621c476d02e930a50b5c5375e
Nathan Wilson <nswilson@gmail.com>
2014-08-20 17:28:19 -0400

    Seed database from real data

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-  validates :first_name, :last_name, presence: true
+  validates :last_name, presence: true
   has_many :books, dependent: :destroy
 
   def last_read
